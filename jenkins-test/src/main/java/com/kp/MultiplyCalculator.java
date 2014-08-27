ca3074cc000ce2378798cdfc436d309f6bfd8617
kumper <kumper@kumper-PC>
2014-08-27 21:22:09 +0200

    asdf

--- jenkins-test/src/main/java/com/kp/MultiplyCalculator.java
+++ jenkins-test/src/main/java/com/kp/MultiplyCalculator.java
@@ -1,14 +1,7 @@
 package com.kp;
 
 public class MultiplyCalculator extends Calculator {
-	
-	/**
-	 * Comment.
-	 * 
-	 * @param a
-	 * @param b
-	 * @return
-	 */
+
 	public int multiply(int a, int b) {
 		return a * b;
 	}
