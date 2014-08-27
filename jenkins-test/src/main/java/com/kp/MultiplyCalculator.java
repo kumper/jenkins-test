a830374d5640439b952b3669ee7a9e977fa6a537
kumper <kumper@kumper-PC>
2014-08-27 21:27:24 +0200

    fdsa

--- jenkins-test/src/main/java/com/kp/MultiplyCalculator.java
+++ jenkins-test/src/main/java/com/kp/MultiplyCalculator.java
@@ -1,9 +1,19 @@
 package com.kp;
 
+import java.rmi.UnexpectedException;
+
 public class MultiplyCalculator extends Calculator {
 
 	public int multiply(int a, int b) {
 		return a * b;
+	}
+	
+	public int divide(int a, int b) throws UnexpectedException {
+		if (b == 0) {
+			throw new UnexpectedException("Division by zero");
+		}
+		
+		return a / b;
 	}
 
 }
