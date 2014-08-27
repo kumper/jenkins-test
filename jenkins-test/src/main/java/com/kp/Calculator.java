55877d2fc69398070a3751460ba6ba48acf11c6a
kumper <kumper@kumper-PC>
2014-08-27 20:28:02 +0200

    Removing those stupid comments

--- jenkins-test/src/main/java/com/kp/Calculator.java
+++ jenkins-test/src/main/java/com/kp/Calculator.java
@@ -1,27 +1,11 @@
 package com.kp;
 
 public class Calculator {
-	
-	/**
-	 * Adding comment.
-	 * 
-	 * @param a
-	 * @param b
-	 * @return
-	 */
 	public int add(int a, int b) {
 		return a + b;
 	}
 	
-	/**
-	 * Subtracting.
-	 * 
-	 * @param a
-	 * @param b
-	 * @return
-	 */
 	public int substract(int a, int b) {
 		return a - b;
 	}
-
 }
